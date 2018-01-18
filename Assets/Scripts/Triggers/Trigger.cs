b9ea91f70b11b0b026c2af34ff39c4aec98bdf07
Richard Moe <richmoe07@gmail.com>
2018-01-17 21:01:44 -0600

    Cool And Good
    frensh foond

---
+++ Assets/Scripts/Triggers/Trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+
+    private bool entered = false;
+    public AudioClip clipp;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!entered)
+        {
+            GetComponent<AudioSource>().PlayOneShot(clipp);
+            entered = true;
+        }
+    }
+}
--- Assets/Scripts/Triggers/portall.cs
+++ Assets/Scripts/Triggers/portall.cs
@@ -4,19 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class portall : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene("after1");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
